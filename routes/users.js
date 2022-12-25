76f779e02efafb0199c31295a82b91b962dc68fc
KevinSlim67 <kevinslim167@gmail.com>
2022-12-25 22:44:59 +0200

    edit profile route added

--- routes/users.js
+++ routes/users.js
@@ -4,11 +4,21 @@ const db = require('./../db');
 
 //Get all users
 router.get('/', async (req, res) => {
-    const sql = `SELECT * FROM User`;
+    const sql = `SELECT id, first_name, last_name, email, phone_number, gender, date_of_birth, country_code FROM User;`;
     db.query(sql, (error, results, fields) => {
         if (error) return console.error(error.message);
 
         res.json(results);
+    });
+});
+
+//Get user with id
+router.get('/:userId', async (req, res) => {
+    const { userId } = req.params;
+    const sql = `SELECT id, first_name, last_name, email, phone_number, gender, date_of_birth, country_code FROM User WHERE id = ?`;
+    db.query(sql, [userId], (error, results, fields) => {
+        if (error) return console.error(error.message);
+        res.json(results[0]);
     });
 });
 
@@ -47,6 +57,30 @@ router.post('/register', async (req, res) => {
     }
 });
 
+//edit user's info
+router.patch('/edit', async (req, res) => {
+    let { user } = req.body;
+
+    const arr = [];
+    for (u in user) {
+        if (u === 'id') continue;
+        if (user[u] === '') user[u] = null;
+        if (user[u] !== null) arr.push(`${u} = '${user[u]}'`);
+    }
+    const sets = arr.join(', ');
+    const sql = `UPDATE USER SET ${sets} WHERE id = ?;`;
+
+    db.query(sql, [user.id], (error, results, fields) => {
+        if (error) {
+            res.send({ status: 'ERROR', message: error.message });
+            console.error(error.message);
+        } else {
+            res.send({ status: 'SUCCESS' });
+        }
+    });
+});
+
+
 //add address to user's account
 router.post('/add-address', async (req, res) => {
     let { name, userId, district, city, streetName, buildingName, floorNumber, roomNumber, landmark, companyName, additionalInfo } = req.body;
@@ -71,7 +105,7 @@ router.get('/addresses/:userId', async (req, res) => {
     const sql = `SELECT * FROM Address WHERE user_id = ?`;
     db.query(sql, [userId], (error, results, fields) => {
         if (error) {
-            console.log(res.json({status: 'ERROR', message: error.message}));
+            console.log(res.json({ status: 'ERROR', message: error.message }));
         }
         res.json(results);
     });
@@ -82,10 +116,10 @@ router.delete('/address/delete', async (req, res) => {
     const { id } = req.body;
     const sql = `DELETE FROM Address WHERE id = ?`;
     db.query(sql, [id], (error, results, fields) => {
-        if (error){
-            res.json({status: 'ERROR'});
+        if (error) {
+            res.json({ status: 'ERROR' });
             return console.error(error.message);
-        } else res.json({status: 'SUCCESS'});
+        } else res.json({ status: 'SUCCESS' });
     });
 });
 
